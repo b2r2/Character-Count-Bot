3c9dcad5141e5e72d35b8d41170ebaf36effb4c4
b2r2 <b2r2@bk.ru>
2018-08-26 21:32:22 +0300

    Added website telegra.ph

--- utils/utils.go
+++ utils/utils.go
@@ -7,13 +7,17 @@ import (
 func IsCorrectURL(url string) bool {
 	correctUrl := []string{
 		"medium",
+		"telegra",
 	}
 	state := false
 	line := strings.Split(url, "://")
 	if len(line) == 2 {
 		line = strings.Split(line[1], ".")
-		if line[0] == correctUrl[0] {
-			state = true
+		for _, curl := range correctUrl {
+			if line[0] == curl {
+				state = true
+				break
+			}
 		}
 	}
 	return state
